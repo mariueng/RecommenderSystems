7d397c77063ce608526f7a006520d6fc4de8da7f
Marius Engen <marius@edyou.no>
2022-03-09 23:54:26 +0100

    EDA and simple content-based recommender system

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ Explicit Matrix Factorization implementation is based on the [blog](https://www.
 Start by looking at the **project_description.ipynb**, and then you can also take a look at the .py scripts if you're interested.
 
 Good luck!
+
+## Setup
+
+To be able to use this project you need an environment supporting installment of the necessary python packages and jupyter notebook (kernels).
+This can be done using pip, pipenv, conda, and jupyter notebook.
+
+### Setup using Pipenv
+
+To get started using pipenv you just have to install pipenv and run
+
+`pipenv shell`
+
+Ensure the correct interpreter is selected in your IDE and you should be ready to go.
+
+NB! You might have to set a kernel for the jupyter notebooks as well manually.
---
+++ eda/tools.py
@@ -0,0 +1,27 @@
+# Exploratory data analasis methods
+
+
+def statistics(df):
+    """
+        Basic statistics based on loaded dataframe
+    """
+    total_num = df.shape[0]
+    
+    print("Total number of events (front page incl.): {}".format(total_num))
+    df.sort_values(by=['userId', 'time'], ascending=True, inplace=True)
+    df_ref = df[df['documentId'].notnull()]
+    num_act = df_ref.shape[0]
+    
+    print("Total number of events (without front page): {}".format(num_act))
+    num_docs = df_ref['documentId'].nunique()
+    
+    print("Total number of documents: {}".format(num_docs))
+    print('Sparsity: {:4.3f}%'.format(float(num_act) / float(1000*num_docs) * 100))
+    df_ref = df_ref.drop_duplicates(subset=['userId', 'documentId']).reset_index(drop=True)
+    print("Total number of events (drop duplicates): {}".format(df_ref.shape[0]))
+    print('Sparsity (drop duplicates): {:4.3f}%'.format(float(df_ref.shape[0]) / float(1000*num_docs) * 100))
+    
+    user_df = df_ref.groupby(['userId']).size().reset_index(name='counts')
+    print("\nDescribe by user:")
+    print(user_df.describe())
+
